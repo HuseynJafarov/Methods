e8ffec690411d2d02e35724f522cb0a1bf4214ee
HuseynJafarov <huseynnj@code.edu.az>
2022-06-21 10:54:46 +0400

    Methods

---
+++ Methoods/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+
+namespace Methoods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1) Verilmiş n -ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın?
+
+            //    Dividable3And7(21);
+
+            // 2) n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın?
+            // Sum(22, 44);
+
+            // 3) Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin?
+
+            //SumOfOdds(1, 10);
+
+            // 4) Verilmish arrayin icindeki tek ededlerin cemini tapin?
+
+            //int[] num = { 1, 3, 8, 12, 36, 5, 7, 9 };
+            //var result = SumOfOddsArr(num);
+            //Console.WriteLine(result);
+
+            // 5) Verilmish arrayin icindeki cut ededlerin sayini tapin?
+
+            //int[] num = { 1, 3, 8, 12, 36, 5, 7, 9 };
+            //var result = SumOddArr(num);
+            //Console.WriteLine(result);
+
+
+            // 6) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin?
+            //SimpleComposite(7);
+
+
+
+            //}
+
+            //1) Verilmiş n -ədədinin 3 - ə və 7 - ə bölünüb - bölünməməsini tapın?
+
+            //static void Dividable3And7(int n)
+            //{
+            //    if (n % 3 == 0 && n % 7 == 0)
+            //    {
+            //        Console.WriteLine("True");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("False");
+            //    }
+            //}
+
+
+            // 2) n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın?
+
+            //static void Sum(int n, int m)
+            //{
+
+            //    if(n % 2 ==0 && m % 2 ==0)
+            //    {
+
+            //        Console.WriteLine(n+m);
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("False");
+            //    }
+
+            //}
+
+
+            // 3) Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin?
+
+            //static void SumOfOdds(int n , int m)
+            //{
+            //    int sum = 0;
+
+            //    for(int i=n+1;i<m;i++)
+            //    {
+            //     if(i%2 ==1)
+            //        {
+            //            sum += i;
+            //            ;
+            //        }
+
+            //    }
+            //    Console.WriteLine(sum);
+
+
+            //}
+
+            // 4) Verilmish arrayin icindeki tek ededlerin cemini tapin?
+
+            //static int SumOfOddsArr(int[] arr)
+            //{
+
+            //    int sum = 0;
+
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+            //        if (arr[i] % 2 == 1)
+            //        {
+            //            sum += arr[i];
+            //        }
+
+
+            //    }
+
+            //    return sum;
+            //}
+
+            //5) Verilmish arrayin icindeki cut ededlerin sayini tapin?
+
+
+
+            //static int SumOddArr(int[] arr)
+            //{
+
+            //    int sum = 0;
+
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+            //        if (arr[i] % 2 == 0)
+            //        {
+            //            sum += arr[i];
+            //        }
+
+
+
+            //    }
+
+            //    return sum;
+            //}
+
+            // 6) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin?
+            //static void SimpleComposite(int n)
+            //{
+            //    int count = 0;
+                
+            //     if(n % 2 == 0)
+            //    {
+            //        Console.WriteLine("Composite");
+
+            //    }
+            //    for(int i = 1; i<=n;i++)
+            //    {
+            //        if(n % i == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    if(count>2)
+            //    {
+            //        Console.WriteLine("composite");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Prime");
+            //    }
+                
+                
+
+
+
+
+
+
+            //}
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+    
+
